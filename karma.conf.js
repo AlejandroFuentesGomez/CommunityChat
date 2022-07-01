1744546e41cfce04fb8ba250028add572c9f2889
Alejandro Fuentes <alejandrofuentes@Mac-mini-de-Alejandro.local>
2022-07-01 11:02:42 +0200

    Init project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/comunity-chat'),
+      dir: require('path').join(__dirname, './coverage/community-chat'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ComunityChat'`, () => {
+  it(`should have as title 'CommunityChat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ComunityChat');
+    expect(app.title).toEqual('CommunityChat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ComunityChat app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CommunityChat app is running!');
   });
 });

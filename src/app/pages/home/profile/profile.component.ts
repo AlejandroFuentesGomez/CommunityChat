840dbb1b8a602210342e51764deb9aa71ab01219
Alejandro_Fuentes_Gómez <alejandro.fuentes@guadaltech.es>
2022-07-11 19:20:05 +0200

    [BUILD] Profile form / The next step is to update user data and change the user profile photo with fireStorage technology

--- src/app/pages/home/profile/profile.component.ts
+++ src/app/pages/home/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { StoreService } from 'src/app/services/store.service';
@@ -12,6 +13,8 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   user:User;
   userSubscription:Subscription;
+  enableForm = true;
+  editProfileForm: FormGroup;
   constructor(private storeService: StoreService) { }
 
   ngOnInit(): void {
@@ -25,8 +28,20 @@ export class ProfileComponent implements OnInit, OnDestroy {
       .getState('userState')
       .subscribe((state) => {
         this.user = state.user;
+        this.fillProfileForm(this.user);
         console.log(this.user);
       });
+  }
+  fillProfileForm(user: User){
+    this.editProfileForm = new FormGroup({
+      email: new FormControl(user.email),
+      name: new FormControl(user.name),
+      surname: new FormControl(user.surname),
+      nick: new FormControl(user.nick),
+      birthdate: new FormControl(user.birthdate),
+      
+
+    })
   }
 
 }
--- src/app/pages/home/profile/profile.module.ts
+++ src/app/pages/home/profile/profile.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ProfileModule { }
